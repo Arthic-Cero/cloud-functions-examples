3e4da22ce9918eef4d1bf31849ac6771bd1b79bd
Arthic-Cero <35507839+Arthic-Cero@users.noreply.github.com>
2018-10-22 14:25:28 -0500

    Update main.py

--- revisions/master/paths/cloud-function/main.py
+++ revisions/master/paths/cloud-function/main.py
@@ -6,6 +6,7 @@ def hello_world(request):
         The response text or any set of values that can be turned into a
         Response object using
         `make_response <http://flask.pocoo.org/docs/0.12/api/#flask.Flask.make_response>`.
+        just making a change to test Cloud Build triggers
     """
     request_json = request.get_json()
     if request.args and 'message' in request.args:
